6439b6e056e7c8c7750f5bb3494924c2e777b2c3
Paulo Cesar <paulo_cesarzn@hotmail.com>
2023-05-31 15:31:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+import './App.css'
+import Calculator from './Componets/Calculator/Calculator'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <div>
+        <h1>Calculadora</h1>
+       <Calculator/>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
